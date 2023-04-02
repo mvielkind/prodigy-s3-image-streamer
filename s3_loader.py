be3753d8b017a73b665cfd7992843b7addc686e1
Matt <matthew.vielkind@gmail.com>
2023-04-01 21:45:35 -0400

    Add before_db function. Check if S3 file was processed previously. Add prodigy interface.

--- s3_loader.py
+++ s3_loader.py
@@ -1,35 +1,77 @@
+from typing import Text, Dict, Any
+from collections.abc import Generator
+
 import boto3
 import prodigy
 import json
 from prodigy.util import img_to_b64_uri
+from prodigy.components.db import connect
+
+
+s3 = boto3.client('s3')
 
 
 @prodigy.recipe("stream-from-s3")
-def stream_from_s3(bucket, prefix=None):
-    # Get all loaded images.
-    s3 = boto3.client('s3')
+def stream_from_s3(dataset: Text, bucket: Text, prefix: Text=None) -> Dict[Text, Any]:
 
-    # Build a paginator for when there are a lot of objects.
-    paginator = s3.get_paginator('list_objects')
-    paginate_params = {
-        'Bucket': bucket
+    def before_db(examples):
+        '''
+        Replaces the raw image data with the S3 key of the image before writing to the Prodigy DB.
+        '''
+        for eg in examples:
+            eg["image"] = eg["meta"]["key"]
+        return examples
+
+    def get_stream() -> Generator:
+        # Build paginator for S3 objects.
+        paginator = s3.get_paginator('list_objects')
+        paginate_params = {
+            'Bucket': bucket
+        }
+
+        if prefix:
+            paginate_params['Prefix'] = prefix
+        
+        page_iterator = paginator.paginate(**paginate_params)
+
+        # Iterate through the pages.
+        for page in page_iterator:
+            # Iterate through items on the page.
+            for obj in page['Contents']:
+                img_key = obj['Key']
+
+                # Skip the record if equal to the prefix.
+                if img_key == f"{prefix}/":
+                    continue
+
+                # Check if the key exists in the database. If so, skip the record.
+                if in_db:
+                    if img_key in in_db:
+                        continue
+
+                # Read the image.
+                _img_bytes = s3.get_object(Bucket=bucket, Key=img_key).get('Body').read()
+
+                yield {'image': img_to_b64_uri(_img_bytes, 'image/jpg'), "meta": {"key": img_key}}
+
+
+    # Load the dataset examples so keys already loaded can be skipped.
+    db = connect()
+    annotated_examples = db.get_dataset(dataset)
+    if annotated_examples:
+        in_db = [d['image'] for d in annotated_examples]
+    else:
+        in_db = None
+
+    # Setup the stream.
+    stream = get_stream()
+
+    # Set your Prodigy interface.
+    return {
+        "view_id": "",
+        "dataset": dataset,
+        "stream": stream,
+        "before_db": before_db,
+        "config": {}
     }
-
-    # Check if only certain images from S3 should be loaded.
-    if prefix is not None:
-        paginate_params['Prefix'] = prefix
-
-    page_iterator = paginator.paginate(**paginate_params)
-
-    # Iterate through the pages.
-    for page in page_iterator:
-        # Iterate through items on the page.
-        for obj in page['Contents']:
-            img_key = obj['Key']
-
-            # Read the image.
-            img = s3.get_object(Bucket=bucket, Key=img_key).get('Body').read()
-
-            # Provide response that Prodigy expects.
-            print(json.dumps({'image': img_to_b64_uri(img, 'image/jpg')}))
 
